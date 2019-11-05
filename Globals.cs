db8db230ae76a0ebafdd5f6e8e46d6c6498af16a
kevL <k3v1s73n@gmail.com>
2019-11-05 11:58:54 -0700

    maintenance

--- Globals.cs
+++ Globals.cs
@@ -24,7 +24,7 @@
 		internal const string Reload = "Reload";
 
 
-		internal const uint BITS_STRREF = 0x00FFFFFF;
+		internal const uint BITS_STRREF = 0x00FFFFFF; // talktable bitflags ->
 		internal const uint BITS_CUSTOM = 0x01000000;
 		internal const uint BITS_UNUSED = 0xFE000000;
 	}
