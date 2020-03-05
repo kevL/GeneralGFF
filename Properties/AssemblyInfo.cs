bd4432e2eec89816f7396795381665e7a324bce8
kevL <k3v1s73n@gmail.com>
2020-03-05 00:23:24 -0700

    0.4.5.0 (signed)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -13,16 +13,16 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: AssemblyCompany("kL")]
 [assembly: AssemblyProduct("GeneralGFF")]
-[assembly: AssemblyCopyright("© 2019 Copyleft")]
+[assembly: AssemblyCopyright("2019-2020 Copyleft")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 //[assembly: Guid("67613839-3b8c-40ab-a48b-9e6b64c43cb2")]
 
-[assembly: AssemblyVersion("0.4.2.0")]
-[assembly: AssemblyFileVersion("0.4.2.0")]
-[assembly: AssemblyInformationalVersion("0.4.2.0")]
+[assembly: AssemblyVersion("0.4.5.0")]
+[assembly: AssemblyFileVersion("0.4.5.0")]
+[assembly: AssemblyInformationalVersion("0.4.5.0")]
 
 //[assembly: System.CLSCompliant(true)]
 
