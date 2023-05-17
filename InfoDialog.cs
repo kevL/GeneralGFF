aa81470707569126e26313bf454fd6835fb6ba84
kevL <k3v1s73n@gmail.com>
2023-05-16 21:06:36 -0600

    InfoDialog - tweak cTor

--- InfoDialog.cs
+++ InfoDialog.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -33,12 +32,12 @@ namespace generalgff
 
 				case Globals.Error:
 					System.Media.SystemSounds.Exclamation.Play();
-					bt_Okay.Text = "Cancel";
+					bu_Okay.Text = "cancel";
 					break;
 			}
 
 
-			int hPadBot = ClientSize.Height - la_Info.Height;
+			SuspendLayout();
 
 			int w = 0, wTest, h = 0, hTest;
 			string[] separators = { "\r\n", "\r", "\n" };
@@ -55,15 +54,15 @@ namespace generalgff
 					h = hTest;
 			}
 			w += la_Info.Padding.Left + la_Info.Padding.Right;
+			if (w < 250) w = 250;
 
-			if (w < bt_Okay.Width)
-				w = bt_Okay.Width;
+			la_Info.Height = h * lines.Length + la_Info.Padding.Top + la_Info.Padding.Bottom;
 
-			la_Info.Height = h * lines.Length + la_Info.Padding.Top;
+			ClientSize = new Size(w, la_Info.Height + bu_Okay.Height);
 
-			ClientSize = new Size(w, la_Info.Height + hPadBot);
+			bu_Okay.Location = new Point(w - bu_Okay.Width - 3, la_Info.Bottom + 6);
 
-			bt_Okay.Location = new Point((w - bt_Okay.Width) / 2, bt_Okay.Top);
+			ResumeLayout();
 		}
 		#endregion cTor
 
@@ -103,27 +102,8 @@ namespace generalgff
 
 
 		#region Designer
-		/// <summary>
-		/// Designer variable used to keep track of non-visual components.
-		/// </summary>
-		IContainer components = null;
-
 		Label la_Info;
-		Button bt_Okay;
-
-
-		/// <summary>
-		/// Disposes resources used by the form.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && components != null)
-				components.Dispose();
-
-			base.Dispose(disposing);
-		}
-
+		Button bu_Okay;
 
 		/// <summary>
 		/// This method is required for Windows Forms designer support.
@@ -133,41 +113,41 @@ namespace generalgff
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.bt_Okay = new System.Windows.Forms.Button();
+			this.bu_Okay = new System.Windows.Forms.Button();
 			this.la_Info = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
-			// bt_Okay
+			// bu_Okay
 			// 
-			this.bt_Okay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.bt_Okay.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.bt_Okay.Location = new System.Drawing.Point(170, 28);
-			this.bt_Okay.Margin = new System.Windows.Forms.Padding(0);
-			this.bt_Okay.Name = "bt_Okay";
-			this.bt_Okay.Size = new System.Drawing.Size(70, 25);
-			this.bt_Okay.TabIndex = 1;
-			this.bt_Okay.Text = "Ok";
-			this.bt_Okay.UseVisualStyleBackColor = true;
+			this.bu_Okay.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.bu_Okay.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.bu_Okay.Location = new System.Drawing.Point(0, 30);
+			this.bu_Okay.Margin = new System.Windows.Forms.Padding(0);
+			this.bu_Okay.Name = "bu_Okay";
+			this.bu_Okay.Size = new System.Drawing.Size(244, 23);
+			this.bu_Okay.TabIndex = 1;
+			this.bu_Okay.Text = "ok";
+			this.bu_Okay.UseVisualStyleBackColor = true;
 			// 
 			// la_Info
 			// 
-			this.la_Info.Dock = System.Windows.Forms.DockStyle.Top;
+			this.la_Info.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.la_Info.ForeColor = System.Drawing.SystemColors.ControlText;
 			this.la_Info.Location = new System.Drawing.Point(0, 0);
 			this.la_Info.Margin = new System.Windows.Forms.Padding(0);
 			this.la_Info.Name = "la_Info";
-			this.la_Info.Padding = new System.Windows.Forms.Padding(5, 5, 6, 0);
-			this.la_Info.Size = new System.Drawing.Size(244, 22);
+			this.la_Info.Padding = new System.Windows.Forms.Padding(5, 6, 8, 5);
+			this.la_Info.Size = new System.Drawing.Size(244, 30);
 			this.la_Info.TabIndex = 0;
 			this.la_Info.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
 			// 
 			// InfoDialog
 			// 
-			this.AcceptButton = this.bt_Okay;
-			this.CancelButton = this.bt_Okay;
-			this.ClientSize = new System.Drawing.Size(244, 56);
+			this.AcceptButton = this.bu_Okay;
+			this.CancelButton = this.bu_Okay;
+			this.ClientSize = new System.Drawing.Size(244, 53);
 			this.Controls.Add(this.la_Info);
-			this.Controls.Add(this.bt_Okay);
+			this.Controls.Add(this.bu_Okay);
 			this.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.ForeColor = System.Drawing.SystemColors.ControlText;
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
